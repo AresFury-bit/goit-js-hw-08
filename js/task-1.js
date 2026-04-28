5e37bfb39e2bfcaab8910aca40e0eb7a9f9579cb
shinobu_kage <maksim.lub@icloud.com>
2026-04-29 00:38:55 +0300

    update 2 hw 7

--- js/task-1.js
+++ js/task-1.js
@@ -2,17 +2,13 @@
 const items = document.querySelectorAll('.item');
 
 const a = () => {
-console.log(`Number of сategories: ${document.querySelectorAll('.item h2').length}`);
+console.log(`Number of сategories: ${items.length}`);
     items.forEach(item => {
         
-        console.log(`Category: ${item.querySelector(`.item h2`).textContent}`);
+        console.log(`Category: ${item.querySelector('h2').textContent}`);
         const li = item.querySelectorAll('li');
         console.log(`Elements: ${li.length}`)
     })
 }  
         
 a();
-
-
-
-
